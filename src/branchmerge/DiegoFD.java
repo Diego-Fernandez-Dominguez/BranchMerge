a4d2abf4ac3aa653a05f91ebfa10ecbf1beec54f
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-01-31 10:38:51 +0100

    Ejercicio Diego

--- src/branchmerge/DiegoFD.java
+++ src/branchmerge/DiegoFD.java
@@ -2,7 +2,7 @@ package branchmerge;
 
 public class DiegoFD {
 	public static void main(String[] args) {
-		System.out.println("Soy DiegoFD");
+		System.out.println("Soy Diego, pero no mucho");
 	}
 
 }
