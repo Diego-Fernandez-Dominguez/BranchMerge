713dc065db8b2dcc689763962c863f6e13ceb83e
AlbaDuqueC <alba.duque@iesnervion.es>
2025-01-31 10:44:34 +0100

    hola cara cola

---
+++ src/branchmerge/AlbDC.java
@@ -0,0 +1,12 @@
+package branchmerge;
+
+public class AlbDC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola cara cola");
+
+	}
+
+}
--- src/branchmerge/ClasePrueba.java
+++
@@ -1,7 +0,0 @@
-package branchmerge;
-
-public class ClasePrueba {
-	public static void main(String[] args) {
-		System.out.println("hola caracola");
-	}
-}
