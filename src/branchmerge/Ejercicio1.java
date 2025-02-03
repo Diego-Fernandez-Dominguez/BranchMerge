609a7549beb41b831da1f680fd362cfb7905ddad
Fg-21 <fm.gonzalez@iesnervion.es>
2025-02-03 13:41:03 +0100

    Push Fran

--- src/branchmerge/Ejercicio1.java
+++ src/branchmerge/Ejercicio1.java
@@ -4,7 +4,7 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		
 		System.out.println(1 + 1);
-		
+		System.out.println("Fran was here");
 	}
 
 }
---
+++ src/branchmerge/Fran.java
@@ -0,0 +1,9 @@
+package branchmerge;
+
+public class Fran {
+
+	public static void comerKebab(int numeroDeKebabs) {
+		int kebabCounter = numeroDeKebabs;
+		System.out.println("Me ha comido" + kebabCounter + "kebab(s)");
+	}
+}
